476971ee9b2975583f88cf0df67118a3a0f88836
Roziya Zohidova <rzohidova8@gmail.com>
2022-10-04 17:33:44 +0500

    add photos

--- src/components/SiteMenu/SiteMenu.vue
+++ src/components/SiteMenu/SiteMenu.vue
@@ -1,21 +1,55 @@
 <template>
-    <div class="menu-wrap">
-      <div class="content">
-        <h1>Welcome</h1>
-        <h2>to our Food site</h2>
-        <p>Menu</p>
-        <p>National foods</p>
-        <p>Fast Food</p>
-        <p>Salads</p>
-        <p>Sweets</p>
-        <p>Drinks</p>
+  <div class="menu-wrap">
+    <div class="page-content">
+      <div class="card">
+        <h1 class="menu-title">Welcome</h1>
+        <h2 class="menu-title">to our Food site</h2>
+        <h3 class="menu-subtitle">Menu</h3>
+        <div v-for="item in menuData"
+        :key="item.id"
+        class="content">
+          <button @click="sortCategory"
+           class="menu-text">{{item.text}}</button>
+        </div>
       </div>
     </div>
+    <div class="menu-left">
+      <div class="menu-img"></div>
+    </div>
+  </div>
 </template>
-
 <script>
 export default {
-
+  name: 'SiteMenuPages',
+  data() {
+    return {
+      menuData: [
+        {
+          id: 1,
+          text: 'National foods',
+        },
+        {
+          id: 2,
+          text: 'Fast Food',
+        },
+        {
+          id: 3,
+          text: 'Salads',
+        },
+        {
+          id: 4,
+          text: 'Sweets',
+        },
+        {
+          id: 5,
+          text: 'Drinks',
+        },
+      ],
+    };
+  },
+  methods: {
+    sortCategory() {},
+  },
 };
 </script>
 
